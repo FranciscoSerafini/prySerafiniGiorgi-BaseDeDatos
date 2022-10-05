6880326cfcf7a7a983fb7d46e641896159f0a16c
FranciscoSerafinGiorgi <franciscoserafinigiorgi@gmail.com>
2022-10-05 15:58:22 -0300

    Cambios en formularios

---
+++ prySerafiniGiorgi-BaseDeDatos/frmPrincipal.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace prySerafiniGiorgi_BaseDeDatos
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.registroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deportistasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entrenadorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deportistasToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.entrenadorToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblEstado = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblFechaActual = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblEstado,
+            this.lblFechaActual});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroToolStripMenuItem,
+            this.consultasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // registroToolStripMenuItem
+            // 
+            this.registroToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deportistasToolStripMenuItem,
+            this.entrenadorToolStripMenuItem});
+            this.registroToolStripMenuItem.Name = "registroToolStripMenuItem";
+            this.registroToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.registroToolStripMenuItem.Text = "Registro";
+            // 
+            // deportistasToolStripMenuItem
+            // 
+            this.deportistasToolStripMenuItem.Name = "deportistasToolStripMenuItem";
+            this.deportistasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deportistasToolStripMenuItem.Text = "Deportistas";
+            // 
+            // entrenadorToolStripMenuItem
+            // 
+            this.entrenadorToolStripMenuItem.Name = "entrenadorToolStripMenuItem";
+            this.entrenadorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.entrenadorToolStripMenuItem.Text = "Entrenador";
+            // 
+            // consultasToolStripMenuItem
+            // 
+            this.consultasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deportistasToolStripMenuItem1,
+            this.entrenadorToolStripMenuItem1});
+            this.consultasToolStripMenuItem.Name = "consultasToolStripMenuItem";
+            this.consultasToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.consultasToolStripMenuItem.Text = "Consultas";
+            // 
+            // deportistasToolStripMenuItem1
+            // 
+            this.deportistasToolStripMenuItem1.Name = "deportistasToolStripMenuItem1";
+            this.deportistasToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.deportistasToolStripMenuItem1.Text = "Deportistas";
+            // 
+            // entrenadorToolStripMenuItem1
+            // 
+            this.entrenadorToolStripMenuItem1.Name = "entrenadorToolStripMenuItem1";
+            this.entrenadorToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.entrenadorToolStripMenuItem1.Text = "Entrenador";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(12, 17);
+            this.lblEstado.Text = "-";
+            // 
+            // lblFechaActual
+            // 
+            this.lblFechaActual.Name = "lblFechaActual";
+            this.lblFechaActual.Size = new System.Drawing.Size(12, 17);
+            this.lblFechaActual.Text = "-";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPrincipal";
+            this.Text = "Pantalla Principal";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblEstado;
+        private System.Windows.Forms.ToolStripStatusLabel lblFechaActual;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem registroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deportistasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entrenadorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deportistasToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem entrenadorToolStripMenuItem1;
+    }
+}
+
---
+++ prySerafiniGiorgi-BaseDeDatos/frmPrincipal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace prySerafiniGiorgi_BaseDeDatos
+{
+    public partial class frmPrincipal : Form
+    {
+        //declaracion de variables y objetos globales
+        //utilizaremos las variables publicas para usarlas en todo el proyecto
+        public OleDbConnection conexionBase; 
+        public OleDbConnection queQuieroDeLaBase;
+        public OleDbConnection lectorDeConsultas;
+
+        public string varRutaDeBaseDeDatos;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
